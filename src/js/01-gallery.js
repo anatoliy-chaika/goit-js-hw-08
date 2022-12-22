30a3c3c7d1774080d274107f94197c4ffc5eba50
anatoliy-chaika <110462151+anatoliy-chaika@users.noreply.github.com>
2022-12-22 13:45:20 +0200

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(SimpleLightbox);
+const gallery = document.querySelector('.gallery');
+
+const createMurcupImages = galleryItems
+  .map(({ preview, original, description }) => {
+    const murcup = `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+    return murcup;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', createMurcupImages);
+
+let slider = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 200,
+});
